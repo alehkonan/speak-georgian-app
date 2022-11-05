a2f4b06dbd31c9045aef78b0b7dfd42c32de2ea5
Oleg Konan <o.konan@softteco.com>
2022-11-05 17:12:31 +0400

    updated service worker

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,28 @@ import { VitePWA } from 'vite-plugin-pwa';
 export default defineConfig({
   plugins: [
     react(),
-    VitePWA({ registerType: 'autoUpdate', devOptions: { enabled: true } }),
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: { enabled: true },
+      workbox: { globPatterns: ['**/*.{js,css,html,ico,png,svg,txt}'] },
+      manifest: {
+        name: 'Speak Georgian App',
+        short_name: 'SpeakGeorgian',
+        description: 'The app to learn and repeat georgian words',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: '192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
   ],
 });
