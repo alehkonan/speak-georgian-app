46a45b167fb6ee15d48da56569f16aafd7dd013a
Oleg Konan <olegkonanff@gmail.com>
2022-12-16 19:04:59 +0400

    added possibility to sign out

--- src/routes/profile/index.tsx
+++ src/routes/profile/index.tsx
@@ -1,7 +1,15 @@
+import { useSignOut } from 'src/features/auth';
+import { Button } from 'src/shared/components';
+
 export const ProfileScreen = () => {
+  const { onSignOut, isLoading } = useSignOut();
+
   return (
     <div>
       <p>Profile screen</p>
+      <Button onClick={onSignOut} disabled={isLoading}>
+        Logout
+      </Button>
     </div>
   );
 };
