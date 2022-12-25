58661deb0219bdabb1e77554261bb333fff3c2b9
Oleg Konan <o.konan@softteco.com>
2022-12-25 16:10:31 +0400

    refactor word card

--- src/shared/components/Card/WordCard.tsx
+++ src/shared/components/Card/WordCard.tsx
@@ -1,20 +1,13 @@
 import classNames from 'classnames';
-import { useState } from 'react';
-import {
-  CopyIcon,
-  EyeIcon,
-  EyeSlashIcon,
-  HeartIcon,
-  SoundIcon,
-} from 'src/shared/icons';
-import { IconButton } from '../Button/IconButton';
+import { useRef, useState } from 'react';
+import { SoundIcon } from 'src/shared/icons';
 
 type Props = {
   nameKa: string;
   nameEn: string;
   transcription: string | null;
   pictureUrl: string | null;
-  soundUrl?: string;
+  soundUrl: string | null;
 };
 
 export const WordCard = ({
@@ -25,46 +18,71 @@ export const WordCard = ({
   soundUrl,
 }: Props) => {
   const [isTranslationShown, setTranslationShown] = useState(false);
+  const audioRef = useRef<HTMLAudioElement | null>(null);
 
   const onCopyWord = async () => {
     await navigator.clipboard.writeText(nameKa);
     alert('Text copied');
   };
 
+  const onPlaySound = () => {
+    if (audioRef.current) {
+      audioRef.current.currentTime = 0;
+      audioRef.current.play();
+    }
+  };
+
   return (
-    <div
-      className={classNames([
-        'bg-white shadow rounded-2xl p-3',
-        'grid grid-cols-[30%_1fr] grid-rows-[auto_1fr_auto] gap-y-1 gap-x-4',
-      ])}
-    >
-      <div className="row-span-3 aspect-square grid place-content-center m-1 p-2 rounded-xl shadow-md">
-        <img src={pictureUrl || undefined} alt={nameEn} />
-      </div>
-      <p>
-        <span className="text-secondary text-lg font-bold">{nameKa}</span>{' '}
-        <span className="text-gray text-xs font-semibold">
-          ({transcription})
-        </span>
-      </p>
-      <div className="h-5">
-        {isTranslationShown && (
-          <span className="text-dark text-sm">{nameEn}</span>
-        )}
-      </div>
-      <div className="flex justify-end items-center gap-3">
-        <IconButton onClick={() => setTranslationShown(!isTranslationShown)}>
-          {isTranslationShown ? <EyeSlashIcon /> : <EyeIcon />}
-        </IconButton>
-        <IconButton onClick={onCopyWord}>
-          <CopyIcon />
-        </IconButton>
-        <IconButton>
-          <HeartIcon />
-        </IconButton>
-        <IconButton>
-          <SoundIcon />
-        </IconButton>
+    <div className={classNames(['bg-white shadow rounded-2xl', 'flex'])}>
+      {pictureUrl && (
+        <div className="flex-shrink-0 basis-1/3">
+          <img
+            className="w-full h-full aspect-square object-cover object-top rounded-l-2xl"
+            src={pictureUrl}
+            alt={nameEn}
+          />
+        </div>
+      )}
+      <div className="flex-1 p-3 grid">
+        <div>
+          <span
+            tabIndex={0}
+            className="text-raisin-black text-xl font-bold cursor-pointer"
+            onClick={onCopyWord}
+          >
+            {nameKa}
+          </span>
+        </div>
+        <div>
+          <div
+            tabIndex={0}
+            className="inline-flex items-center gap-2 cursor-pointer"
+            onClick={onPlaySound}
+          >
+            <span className="text-raisin-black opacity-50">
+              ({transcription || 'no transcription'})
+            </span>
+            {soundUrl && (
+              <div>
+                <SoundIcon className="w-5 h-5 text-raisin-black opacity-50" />
+                <audio ref={audioRef} src={soundUrl}></audio>
+              </div>
+            )}
+          </div>
+        </div>
+        <div className="h-7">
+          {isTranslationShown ? (
+            <span className="text-raisin-black opacity-50">{nameEn}</span>
+          ) : (
+            <span
+              tabIndex={0}
+              className="text-sm font-semibold opacity-30 cursor-pointer"
+              onClick={() => setTranslationShown(true)}
+            >
+              Tap to see translation
+            </span>
+          )}
+        </div>
       </div>
     </div>
   );
