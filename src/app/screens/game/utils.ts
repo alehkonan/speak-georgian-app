3402665798da4bc287330db6cb0405461d731658
Oleg Konan <olegkonanff@gmail.com>
2023-01-06 19:13:03 +0400

    update useGame functionality

---
+++ src/app/screens/game/utils.ts
@@ -0,0 +1,37 @@
+import { Word } from 'src/shared/types';
+import { generateRandomElements, getRandomInteger } from 'src/shared/utils';
+
+export const generateAnswers = (
+  rightAnswer: string,
+  possibleAnswers: string[]
+) => {
+  const answers = new Set<string>();
+
+  answers.add(rightAnswer);
+
+  for (let i = 0; i < 3; i++) {
+    const getRandomWord = () => {
+      const index = getRandomInteger(0, possibleAnswers.length);
+      return possibleAnswers[index];
+    };
+    let word = getRandomWord();
+    while (answers.has(word)) {
+      word = getRandomWord();
+    }
+    answers.add(word);
+  }
+
+  return generateRandomElements([...answers]);
+};
+
+export const generateGameWords = (allWords: Word[], count: number) => {
+  if (!allWords.length) return [];
+
+  const answers = allWords.map((word) => word.name_en);
+  const randomWords = generateRandomElements<Word>(allWords, count);
+
+  return randomWords.map((word) => ({
+    ...word,
+    answers: generateAnswers(word.name_en, answers),
+  }));
+};
--- src/shared/utils/index.ts
+++ src/shared/utils/index.ts
@@ -18,3 +18,17 @@ export const formatDate = (date: string | null) => {
 export const getRandomInteger = (min: number, max: number) => {
   return Math.floor(Math.random() * (max - min)) + min;
 };
+
+/**
+ * function to generate random elements from an array
+ * @param elements
+ * @param {Number} count number of elements to return
+ * @returns array of elements in random order
+ */
+export const generateRandomElements = <Element = string>(
+  elements: Element[],
+  count?: number
+) => {
+  const shuffled = [...elements].sort(() => Math.random() - 0.5);
+  return shuffled.slice(0, count);
+};
