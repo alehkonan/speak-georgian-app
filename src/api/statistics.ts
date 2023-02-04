2a0ca627f93cfbf4e2ad9ed0c3001c76f8a41ea2
Oleg Konan <o.konan@softteco.com>
2023-02-04 21:01:51 +0400

    fix stat invalidation after mark word as learned

--- src/api/statistics.ts
+++ src/api/statistics.ts
@@ -6,7 +6,10 @@ export const useStatistics = () => {
   const queryClient = useQueryClient();
   const { mutate, isLoading } = useMutation({
     mutationFn: (wordId: number) => setWordAsLearned(wordId),
-    onSuccess: () => queryClient.invalidateQueries(apiKeys.words),
+    onSuccess: () => {
+      queryClient.invalidateQueries(apiKeys.words);
+      queryClient.invalidateQueries(apiKeys.userStatistics);
+    },
   });
 
   const markAsLearned = (wordId: number) => mutate(wordId);
