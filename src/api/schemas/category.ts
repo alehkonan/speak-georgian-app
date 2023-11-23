a0059e7288219df58359fd2bc0b06a79111f80dc
Aleh <olegkonanff@gmail.com>
2023-11-23 13:34:07 +0400

    updated schema categories

--- src/api/schemas/category.ts
+++ src/api/schemas/category.ts
@@ -2,9 +2,9 @@ import zod from 'zod';
 
 export const CategorySchema = zod.object({
   id: zod.number(),
-  name: zod.string(),
-  picture_url: zod.string().nullish(),
-  created_at: zod.string().datetime({ offset: true }),
+  name_en: zod.string(),
+  picture_url: zod.string().nullable(),
+  created_at: zod.string().datetime(),
 });
 
 export type Category = zod.infer<typeof CategorySchema>;
