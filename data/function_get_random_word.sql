ddfd9a6d17a29f782c0bdbda4877749fe0e0210a
Aleh <olegkonanff@gmail.com>
2023-12-05 18:29:45 +0400

    update random word function

--- data/function_get_random_word.sql
+++ data/function_get_random_word.sql
@@ -2,11 +2,15 @@ CREATE OR REPLACE FUNCTION get_random_word()
   RETURNS SETOF words
   AS $$
   SELECT
-    *
+    w.*
   FROM
-    words
-  ORDER BY
-    random()
-  LIMIT 1;
+    words w
+  LEFT JOIN statistic s ON w.id = s.word_id
+WHERE
+  s.is_learned IS NULL
+  OR s.is_learned = FALSE
+ORDER BY
+  random()
+LIMIT 1;
 $$
 LANGUAGE SQL;
